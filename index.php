6af9e818e1aa792f9b728514856d7ccdffc75ebd
Abdul Mumin <mumin.rakib@gmail.com>
2019-01-28 00:35:06 +0600

    First time added

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+
+$firstname= " Abdul";
+$lastname= "mumin";
+$age= 27;
+
+
+echo ' My customer name is $firstname                    $lastname. His age is $age ';
+
+
+/*$result= $number_one + $number_two;
+
+
+
+echo $result; */
+
+
