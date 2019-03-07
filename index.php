fd2a5a5fbcbdce534b195b5e96f2a1ff9e44e14d
Abdul Mumin <mumin.rakib@gmail.com>
2019-03-08 02:51:10 +0600

    1

--- index.php
+++ index.php
@@ -1,9 +1,84 @@
 <?php
 
 
-$x  = 23986589147653495648956;
+$name  = "abdul";
+$phone = 789;
+$number = 100; 
+$other = $phone+$number; 
 
-echo $x;
+/*echo $name; 
+echo "<br>";
+echo $phone; echo "<br>";
+echo $number; echo "<br>";
+echo $other; 
+echo "<br>";
+
+echo $name.$phone.$number;
+
+$new = $name.$phone.$number;
+echo "<br>";
+*/
+
+$r=32;
+
+$PI=3.1416;
+
+
+$area= $PI*$r*$r;
+
+//echo "Result is: $area"."<br>";
+
+
+/*if($number => 100)
+{
+    echo "Your number is greater than 100"."<br>";
+}
+else
+{
+    echo "the number less thab 100"."<br>";
+} 
+
+
+$x = 1;
+
+
+$x = 5;
+
+
+
+$sum = 0;
+for($i = 0; $i<101; $i = $i+1)
+{
+        $sum = $sum + $i; 
+        
+}
+
+echo $sum; 
+        echo "<br>";
+*/
+
+$test = 49;
+
+if ($test < 50)
+{
+    echo "result is F";
+}
+
+elseif ($test >= 50 && $test <80)
+{
+    echo "result is B";
+}
+elseif ($test >=80 && $test <= 100)
+{
+    echo "result is A";
+}
+else 
+{
+    echo "input is incorrect";
+}
+
+
+
 
 
 
